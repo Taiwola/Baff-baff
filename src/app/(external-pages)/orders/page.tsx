fd9db4af09fb3ecaa81fc888a5b58bbc24bf734e
Intuneteq <tobiolanitori@gmail.com>
2025-09-08 07:35:31 +0100

    feat: orders responsiveness

--- src/app/(external-pages)/orders/page.tsx
+++ src/app/(external-pages)/orders/page.tsx
@@ -11,12 +11,16 @@ import { BreadCrumbItemType, BreadCrumbs } from '@components/ui'
 export default function Orders() {
    return (
       <main className='app-container py-5 md:py-12 font-montserrat'>
-         <BreadCrumbs
-            separator={<ChevronRightIcon className='text-base text-black w-4 h-4' />}
-            items={items}
-         />
+         <div className='w-full hidden md:block'>
+            <BreadCrumbs
+               separator={<ChevronRightIcon className='text-base text-black w-4 h-4' />}
+               items={items}
+            />
+         </div>
 
-         <div className='mx-auto max-w-[65%] flex flex-col justify-center items-start mt-5'>
+         <h2 className='md:hidden font-semibold text-black text-[1.125rem]'>ORDER HISTORY</h2>
+
+         <div className='mx-auto w-full md:max-w-[65%] flex flex-col justify-center items-start mt-5'>
             <div className="mx-auto w-full max-w-[60rem] flex flex-col gap-8">
                {orders.map((order) => (
                   <OrderItem key={order.id} order={order} />
@@ -29,10 +33,10 @@ export default function Orders() {
 
 function OrderItem({ order }: { order: Order }) {
    return (
-      <div className="w-full border border-foreground rounded-lg overflow-hidden">
+      <div className="w-full border border-foreground md:rounded-lg overflow-hidden">
          {/* Header */}
-         <div className="flex justify-start items-center px-7.5 py-5 border-b border-foreground gap-5">
-            <div className='text-xs text-brand-dark flex flex-col justify-start items-start'>
+         <div className="flex justify-start items-center p-4 md:px-7.5 md:py-5 border-b border-foreground gap-5 md:gap-5">
+            <div className='text-[10px] md:text-xs text-brand-dark flex flex-col justify-start items-start'>
                <span className='font-semibold'>Order number</span>
                <span>{order.reference}</span>
             </div>
@@ -60,10 +64,10 @@ function OrderItem({ order }: { order: Order }) {
 
 function ProductItem({ product }: { product: Product }) {
    return (
-      <div className="flex items-start justify-between gap-4 p-4 h-full">
-         <div className='flex justify-start items-start gap-7.5'>
+      <div className="flex flex-col md:flex-row items-start justify-between gap-4 p-4 h-full">
+         <div className='flex justify-start items-start gap-4 md:gap-7.5'>
             {/* Image */}
-            <div className="w-[9.375rem] h-[9.375rem] rounded-[20px] overflow-hidden flex-shrink-0">
+            <div className="w-[7.5rem] h-[7.5rem] md:w-[9.375rem] md:h-[9.375rem] rounded-[20px] overflow-hidden flex-shrink-0">
                <Image
                   src={product.images[0]}
                   alt={product.name}
@@ -74,19 +78,19 @@ function ProductItem({ product }: { product: Product }) {
             </div>
 
             <div className="flex flex-col">
-               <span className="text-[1.125rem] text-black">{product.name}</span>
-               <span className="text-base text-black font-bold">{formatCurrency(product.price)}</span>
+               <span className="text-base md:text-[1.125rem] text-black">{product.name}</span>
+               <span className="text-sm md:text-base text-black font-semibold md:font-bold">{formatCurrency(product.price)}</span>
             </div>
          </div>
 
          {/* Info + Actions */}
 
-         <div className="flex items-center justify-center gap-4 h-full mt-auto text-[#008080]">
-            <Link href={`/products/${product.id}`} className="text-sm text-[#008080] font-bold">
+         <div className="flex items-center justify-between md:justify-center md:gap-4 h-full mt-auto text-[#008080] w-full md:w-auto">
+            <Link href={`/products/${product.id}`} className="text-[10px] md:text-sm text-[#008080] font-bold">
                View Product
             </Link>
-            <span>|</span>
-            <button className="text-sm font-bold">Buy Again</button>
+            <span className='hidden md:block'>|</span>
+            <button className="text-[10px] text-sm font-bold">Buy Again</button>
          </div>
       </div>
    )
