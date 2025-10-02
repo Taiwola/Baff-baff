628e8068c763a01742e70d3bb17355093971df9d
taiwola <seunolanitori@gmail.com>
2025-10-02 11:09:55 +0100

    update: forget and reset password

---
+++ src/app/api/v1/auth/forgot-password/route.ts
@@ -0,0 +1,56 @@
+import dbConnect from '@lib/database'
+import { sendEmail } from '@lib/mail'
+import { encrypt } from '@lib/session'
+import { getUserByEmail } from '@services/user'
+import { errorResponse, sendResponse } from '@utils/api-response'
+import { generateResetPasswordEmail } from '@utils/mail-content'
+import { forgotPasswordSchema } from '@validations/forgot-password/create-forgot-password.validation'
+import { NextRequest } from 'next/server'
+
+async function loadDb() {
+  await dbConnect()
+}
+
+loadDb()
+
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+
+  try {
+    const result = forgotPasswordSchema.safeParse(body)
+
+    if (!result.success) {
+      const validationErrors = result.error.issues.map((detail) => ({
+        field: detail.path.join('.'),
+        message: detail.message
+      }))
+
+      return errorResponse('Validation failed', validationErrors, 422)
+    }
+
+    const user = await getUserByEmail(result.data.email)
+
+    if (!user) {
+      return errorResponse('User not found', null, 404)
+    }
+
+    const token = await encrypt({
+      id: user.id,
+      role: user.role,
+      expiresAt: new Date(Date.now() + 60 * 60 * 1000)
+    })
+    const resetLink = `${process.env.CLIENT_BASE_URL}/reset-password?token=${token}`
+    const content = generateResetPasswordEmail({ email: user.email, name: user.firstName }, resetLink)
+
+    const { error, errorMessage } = await sendEmail(user.email, content, 'Reset Your Password', 'Baffa Baffa')
+    if (error) {
+      console.error('Failed to send reset email:', errorMessage)
+      return errorResponse('Failed to send reset email. Please try again.', null, 500)
+    }
+
+    return sendResponse('Reset Link sent', null, 200)
+  } catch (error) {
+    console.error('Login error:', error)
+    return errorResponse('Error logging in user', process.env.NODE_ENV === 'development' ? error : null, 500)
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,13 @@ export async function middleware(request: NextRequest) {
   if (path === '/api/v1/products' && method === 'GET') {
     return NextResponse.next()
   }
+  if (path === '/api/v1/forget-password' && method === 'POST') {
+    return NextResponse.next()
+  }
+
+  if (path === '/api/v1/reset-password' && method === 'PATCH') {
+    return NextResponse.next()
+  }
 
   if (path === '/api/v1/webhooks' && method === 'POST') {
     return NextResponse.next()
@@ -27,7 +34,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.nextUrl))
   }
 
-  if(path.includes('/dashboard') && session?.role !== 'admin') {
+  if (path.includes('/dashboard') && session?.role !== 'admin') {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
 
