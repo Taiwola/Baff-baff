d12e3de9388f4a1b7183eb8848cbc90445a053e9
Intuneteq <tobiolanitori@gmail.com>
2025-10-28 03:41:33 +0000

    feat: google login

--- src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { deleteSession } from '@lib/session'
-import { sendResponse } from '@utils/api-response'
-
-export async function DELETE() {
-  await deleteSession()
-  return sendResponse('Logout successful')
-}
